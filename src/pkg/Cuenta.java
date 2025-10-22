c4b36b72d982257bf76284732009ae8c4c3c7a27
jaime <jaime@DESKTOP-2DSTK2S>
2025-10-22 12:12:46 +0200

    Version_1_Prueba_falla

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,12 @@
+package pkg;
+
+import java.util.List;
+
+public class Cuenta {
+	String mNumero; 
+    String nTitular; 
+    List <Movimiento> mMovimientos;
+
+public void ingresar(double x) {}
+public void retirar(double x) {}
+}
